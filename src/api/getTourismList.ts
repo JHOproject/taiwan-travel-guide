28ddc89166b460db6ffbfdd28abac5284f652e84
jessie <jessie@sjis.com.tw>
2021-11-04 17:18:27 +0800

    [ref] 排版

--- src/api/getTourismList.ts
+++ src/api/getTourismList.ts
@@ -1,9 +1,18 @@
 import $axios from '@/plugins/axios'
 
-const API = {
-  getCityScenicSpotList: '/v2/Tourism/ScenicSpot/',
+/**
+ * 取得所有觀光景點資料
+ * @returns
+ */
+export const getScenicSpotList = (query: string = '') => {
+  return $axios.get('/v2/Tourism/ScenicSpot' + query)
 }
 
-export const getCityScenicSpotList = (params: string) => {
-  return $axios.get(API.getCityScenicSpotList + params) // 取得指定[縣市]觀光景點資料
+/**
+ * 取得指定[縣市]觀光景點資料
+ * @param params city
+ * @returns
+ */
+export const getCityScenicSpotList = (params: string, query: string = '') => {
+  return $axios.get(`/v2/Tourism/ScenicSpot/${params}` + query)
 }
--- src/store/app.ts
+++ src/store/app.ts
@@ -1,37 +1,31 @@
-import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators"
-import store from "./index"
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
+import store from './index'
 
-import { getCityScenicSpotList } from "@/api/getTourismList"
-import { IScenicSpotTourismInfo } from "@/models/tourism"
-import { IResponse } from "@/models/common/Response"
-import { IMap } from "@/models/common/Map"
+import { getCityScenicSpotList } from '@/api/getTourismList'
+import { IScenicSpotTourismRes } from '@/models/TourismDTO'
+import { IResponse } from '@/models/common/Response'
+import { IMap } from '@/models/common/Map'
 
 @Module({
   namespaced: true,
   dynamic: true,
   store,
   stateFactory: true,
-  name: "app",
+  name: 'app',
 })
 export default class App extends VuexModule {
-  cityList = [...require("@/setting/city.json").list]
-  cityScenicSpotMap: IMap<IScenicSpotTourismInfo> = {}
+  cityList = [...require('@/setting/city.json').list]
+  cityScenicSpotMap: IMap<IScenicSpotTourismRes> = {}
 
   @Mutation
-  private SET_MAP({
-    key,
-    data,
-  }: {
-    key: string
-    data: IScenicSpotTourismInfo
-  }) {
+  private SET_MAP({ key, data }: { key: string; data: IScenicSpotTourismRes }) {
     this.cityScenicSpotMap[key] = data
   }
 
   @Action
   public async getCityScenicSpotList(params: string) {
     return getCityScenicSpotList(params)
-      .then(({ status, data }: IResponse<IScenicSpotTourismInfo>) => {
+      .then(({ status, data }: IResponse<IScenicSpotTourismRes>) => {
         if (status === 200) {
           this.SET_MAP({ key: params, data })
         } else {
