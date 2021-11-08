8e39a49a05866371b3848919b9f40de842b4d20f
JHOproject <yabe55230@gmail.com>
2021-11-09 01:35:21 +0800

    [rft] 重構css

--- src/components/utils/Header.vue
+++ src/components/utils/Header.vue
@@ -1,53 +1,19 @@
 <template>
-  <div
-    class="
-      flex
-      justify-between
-      items-center
-      w-full
-      h-24
-      px-10
-      sticky
-      top-0
-      z-10
-      bg-white
-    "
-  >
-    <div class="w-40 h-10 relative z-20" @click="$router.push({ path: '/' })">
-      <img src="@/assets/img/logo.png" alt="" />
-    </div>
+  <div class="navGroup">
     <div
-      class="
-        flex
-        justify-center
-        items-center
-        w-40
-        h-8
-        relative
-        z-20
-        rounded-full
-        bg-gray-100
-      "
+      class="flex w-40 h-10 relative z-20"
+      @click="$router.push({ path: '/' })"
     >
+      <img src="@/assets/img/utils/logo.png" alt="" />
+    </div>
+    <div class="searchGroup">
       <input
         placeholder="搜尋關鍵字..."
         class="w-full pl-4 bg-transparent border-0 outline-none"
       />
       <img src="" alt="" />
     </div>
-    <div
-      class="
-        flex
-        justify-center
-        w-full
-        h-24
-        items-center
-        space-x-8
-        absolute
-        top-0
-        left-0
-      "
-    >
+    <div class="btnGroup">
       <router-link to="/ScenicSpot">景點</router-link>
       <router-link to="/restaurant">美食</router-link>
       <router-link to="/hotel">住宿</router-link>
@@ -63,4 +29,14 @@ import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
 export default class Header extends Vue {}
 </script>
 
-<style lang="sass"></style>
+<style scope>
+.navGroup {
+  @apply flex justify-between items-center w-full h-24 px-10  sticky top-0 z-10 bg-white;
+}
+.searchGroup {
+  @apply flex justify-center items-center w-40 h-8 relative z-20 rounded-full bg-gray-100;
+}
+.btnGroup {
+  @apply flex justify-center w-full h-24 items-center space-x-8 absolute top-0 left-0;
+}
+</style>
