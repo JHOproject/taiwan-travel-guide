98d3d45cbb6b2d4470c1e17ad6c6145d428998f6
jessie <jessie@sjis.com.tw>
2021-11-08 18:44:26 +0800

    [add] 新增InfoCard組件

---
+++ src/components/common/InfoCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="(item, index) in list" :key="index">
+      <span>{{ item.Name }}</span>
+      <img :src="item.Picture.PictureUrl1" height="80" />
+      <img :src="item.Picture.PictureUrl2" height="80" />
+      <img :src="item.Picture.PictureUrl3" height="80" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
+import { IRestaurantInfoItem, IScenicSpotInfoItem } from '@/models/TourismDTO'
+
+@Component({})
+export default class InfoCard extends Vue {
+  @Prop() readonly list!: IScenicSpotInfoItem[] | IRestaurantInfoItem[]
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
+
