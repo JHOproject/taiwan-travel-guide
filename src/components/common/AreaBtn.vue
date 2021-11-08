303947c409730c1d5ab8b16e6b0924310d0ae588
JHOproject <yabe55230@gmail.com>
2021-11-09 01:34:58 +0800

    [add] 新增區域按鈕

---
+++ src/components/common/AreaBtn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="buttonGroup">
+    <button class="imgWrap rounded-full w-20 h-20 mb-2">
+      <img :src="src" :alt="alt" class="w-12 h-12 m-auto" />
+    </button>
+    <span>{{ alt }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
+
+@Component({
+  components: {},
+})
+export default class AreaBtn extends Vue {
+  @Prop() readonly src!: string
+  @Prop() readonly alt!: string
+}
+</script>
+
+<style scoped>
+.buttonGroup {
+  @apply flex flex-col justify-center items-center w-28 h-36 pt-5 rounded-2xl shadow-xl cursor-pointer;
+}
+.imgWrap {
+  background: linear-gradient(155.4deg, #78e389 0.2%, #1eb893 90.81%);
+}
+</style>
---
+++ src/components/utils/AreaBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container mx-auto flex space-x-10">
+    <p class="text-2xl font-bold">縣市快選</p>
+    <AreaBtn
+      v-for="item in list"
+      :key="item.key"
+      :src="item.src"
+      :alt="item.alt"
+    ></AreaBtn>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
+import AreaBtn from '@/components/common/AreaBtn.vue'
+
+@Component({
+  components: { AreaBtn },
+})
+export default class AreaBar extends Vue {
+  list = [
+    { key: 'north', src: require('@/assets/img/utils/north.png'), alt: '北部' },
+    { key: 'mid', src: require('@/assets/img/utils/mid.png'), alt: '中部' },
+    { key: 'south', src: require('@/assets/img/utils/south.png'), alt: '南部' },
+    { key: 'east', src: require('@/assets/img/utils/east.png'), alt: '東部' },
+    {
+      key: 'island',
+      src: require('@/assets/img/utils/island.png'),
+      alt: '離島',
+    },
+  ]
+}
+</script>
