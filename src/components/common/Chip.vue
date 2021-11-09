307f2cea895085d59771bd2a6866fc0341a9614a
jessie <jessie@sjis.com.tw>
2021-11-09 18:45:57 +0800

    [add] 建立元件

---
+++ src/components/common/Chip.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="py-2 px-4 rounded-full text-xs font-bold" :class="className">{{
+    text
+  }}</span>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
+
+@Component({
+  components: {},
+})
+export default class Chip extends Vue {
+  @Prop() text!: string
+  @Prop({ default: 'bg-gray-100' }) className?: string
+}
+</script>
---
+++ src/components/utils/TitleBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="textGroup">
+    <p class="text-3xl font-bold whitespace-pre-wrap" v-html="title"></p>
+    <p class="text-base text-gray-400">{{ subTitle }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
+
+@Component({
+  components: {},
+})
+export default class TitleBar extends Vue {
+  @Prop() readonly title!: string
+  @Prop() readonly subTitle!: string
+}
+</script>
+
+<style scoped>
+.textGroup {
+  @apply mr-12 !important;
+}
+</style>
