466c11f641eb91b91f166302fee49f95de684a92
jessie <jessie@sjis.com.tw>
2021-11-12 16:44:30 +0800

    [add] ScenicSpot 串接資料

--- src/views/ScenicSpot.vue
+++ src/views/ScenicSpot.vue
@@ -1,59 +1,130 @@
 <template>
   <div class="container mx-auto">
-    <div v-if="cityList">
-      <select v-model="selected" @change="onSelectChnage">
-        <option
-          v-for="(item, index) in cityList"
-          :key="index"
-          :value="item.value"
-        >
-          {{ item.text }}
-        </option>
-      </select>
-      <!-- <InfoCard :list="scenicSpotList"></InfoCard>
-      <InfoCard :list="restaurantList"></InfoCard> -->
+    <p class="text-3xl font-bold mt-20 w-10/12 mx-auto">熱門景點</p>
+    <AreaHorizontalBar
+      class="my-12"
+      @onAreaChange="onAreaChange"
+      @onCityChange="onAreaChange"
+    ></AreaHorizontalBar>
+
+    <div
+      v-if="list && endIndex"
+      class="flex flex-wrap justify-center w-10/12 mx-auto"
+    >
+      <InfoCard
+        v-for="(item, index) in list.slice(startIndex, endIndex)"
+        :item="item"
+        :description="item.DescriptionDetail"
+        :classify="item.Class1"
+        :key="index"
+        class="mx-3"
+      ></InfoCard>
     </div>
+
+    <div
+      v-if="list && endIndex"
+      class="flex justify-center w-10/12 mx-auto my-12"
+    >
+      <PaginationBar
+        :page="page"
+        :totalCount="pageCount"
+        @onPageChange="onPageChange"
+        ref="PaginationBar"
+      ></PaginationBar>
+    </div>
+
+    <div></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
-// import InfoCard from '@/components/common/InfoCard.vue'
+import InfoCard from '@/components/common/InfoCard.vue'
+import AreaHorizontalBar from '@/components/utils/AreaHorizontalBar.vue'
+import PaginationBar from '@/components/utils/PaginationBar.vue'
 
-import {
-  getCityRestaurantList,
-  getCityScenicSpotList,
-} from '@/api/getTourismList'
-import { IRestaurantInfoItem, IScenicSpotInfoItem } from '@/models/TourismDTO'
+import { getCityScenicSpotList } from '@/api/getTourismList'
+import { IScenicSpotInfoItem } from '@/models/TourismDTO'
 import { ISelect } from '@/models/common/FormDTO'
 import { IResponse } from '@/models/common/ResponseDTO'
 
-const cityList = require('@/setting/city.json')
+const cityData = require('@/setting/city.json')
 
 @Component({
   components: {
-    // InfoCard,
+    InfoCard,
+    AreaHorizontalBar,
+    PaginationBar,
   },
 })
 export default class ScenicSpot extends Vue {
-  cityList: ISelect[] = []
-  selected = ''
-  scenicSpotList: IScenicSpotInfoItem[] = []
-  restaurantList: IRestaurantInfoItem[] = []
+  SHOW_COUNT = 12
+  startIndex = 0
+  endIndex = 0
 
-  created() {
-    this.cityList = cityList.list
-    this.selected = this.cityList[0].value
-    // this.getCityScenicSpotList(this.selected)
-    // this.getCityRestaurantList(this.selected)
+  areaList: ISelect[] = []
+  cityList: ISelect[] = []
+  list: IScenicSpotInfoItem[] = []
+
+  get area(): string {
+    return <string>this.$route.query.area || this.areaList[0]?.value
+  }
+  get city(): string {
+    return <string>this.$route.query.city || this.cityList[0]?.value
+  }
+  get cityName(): string {
+    return this.cityList.find((x) => x.value === this.city)?.text || ''
+  }
+  get page(): number {
+    return +(<string>this.$route.query.page) || 1
+  }
+  get pageCount(): number {
+    return Math.ceil(this.list.length / this.SHOW_COUNT)
   }
 
-  getCityScenicSpotList(params: string) {
+  @Watch('city')
+  cityChange() {
+    this.getCityScenicSpotList()
+  }
+  @Watch('page')
+  pageChange(val: number) {
+    this.startIndex = (val - 1) * this.SHOW_COUNT
+    this.endIndex = this.startIndex + this.SHOW_COUNT
+  }
+
+  created() {
+    this.areaList = cityData.areaList
+    this.cityList = cityData.levelData[this.area]
+    ;(!this.$route.query.area ||
+      !this.$route.query.city ||
+      !this.$route.query.page) &&
+      this.$router.push({
+        query: {
+          area: this.areaList[0]?.value,
+          city: this.cityList[0]?.value,
+          page: '1',
+        },
+      })
+  }
+
+  mounted() {
+    this.startIndex = (this.page - 1) * this.SHOW_COUNT
+    this.endIndex = this.startIndex + this.SHOW_COUNT
+    this.getCityScenicSpotList()
+  }
+
+  getCityScenicSpotList(params: string = this.city) {
     if (!params) return
     getCityScenicSpotList(params)
       .then(({ status, data }: IResponse<IScenicSpotInfoItem[]>) => {
         if (status === 200) {
-          this.scenicSpotList = data
+          this.list = data.sort((a, b) => {
+            return !!a.Picture?.PictureUrl1 && !b.Picture?.PictureUrl1
+              ? -1
+              : !a.Picture?.PictureUrl1 && !!b.Picture?.PictureUrl1
+              ? 1
+              : 0
+          })
         } else {
           console.log(data)
         }
@@ -63,23 +134,21 @@ export default class ScenicSpot extends Vue {
       })
   }
 
-  getCityRestaurantList(params: string) {
-    if (!params) return
-    getCityRestaurantList(params)
-      .then(({ status, data }: IResponse<IRestaurantInfoItem[]>) => {
-        if (status === 200) {
-          this.restaurantList = data
-        } else {
-          console.log(data)
-        }
-      })
-      .catch((res) => {
-        console.log(res)
-      })
+  onPageChange(page: number) {
+    if (page === +(<string>this.$route.query.page)) return
+    this.$router.push({
+      query: { area: this.area, city: this.city, page: page + '' },
+    })
   }
 
-  onSelectChnage() {
-    this.getCityScenicSpotList(this.selected)
+  onAreaChange() {
+    this.cityList = cityData.levelData[this.area]
+    ;(<PaginationBar>this.$refs.PaginationBar).init()
+  }
+
+  init() {
+    this.startIndex = 0
+    this.endIndex = this.SHOW_COUNT
   }
 }
 </script>
